978716d5cb8a568871e8aa7cc0c749f8fb0a9066
jandresleiva <andresleiva.4@gmail.com>
2020-06-29 12:28:06 -0300

    Adds MySQL connector class

---
+++ Data/DBConnect.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCRALoader.Data
+{
+    public class DBConnection
+    {
+        //private DBConnection()
+        //{
+        //}
+
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        //private static DBConnection _instance = null;
+        //public static DBConnection Instance()
+        //{
+        //    if (_instance == null)
+        //        _instance = new DBConnection();
+        //    return _instance;
+        //}
+
+        public bool IsConnect(int dbase = 0)
+        {
+            if (Connection == null)
+            {
+                string connstring;
+                connstring = string.Format("Server=localhost; database=digi_new; UID=updater; password=as2Kjl1M;SslMode=none");
+
+                connection = new MySqlConnection(connstring);
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+            connection = null;
+        }
+    }
+}
