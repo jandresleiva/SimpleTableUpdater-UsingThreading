6c72dcaeddb77302e281842a7657be35fed1fbc7
jandresleiva <andresleiva.4@gmail.com>
2020-06-29 13:25:14 -0300

    Implementation of the base program.
    This uses Parallel in order to increase updating speed, with a couple of variables to controll the flow.
    Implements Updater Class.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+using Update_Clients_CUIL.Data;
+
+namespace Update_Clients_CUIL
+{
+    class Program
+    {
+        // This program iterates through a table and updates values. The Updater class has a static member DoUpdateAsync
+        // responsible of the transformation.
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // This var holds the max number of rows in the table to update.
+            var qtyRows = 71179;
+            //var qtyRows = 100;
+
+            ParallelOptions po = new ParallelOptions();
+            // This property determines the maximum number of parallel tasks
+            // WARNING: I noted that with a degree over 20 the mysql server gets unresponsive for my specific build. Higher values will usually result in faster results.
+            po.MaxDegreeOfParallelism = 20;
+            Parallel.For(0, (int)qtyRows, po, async (j) => {
+                await Updater.DoUpdateAsync(j, 1);
+            });
+
+        }
+    }
+}
