365f2a29bce4d1e5a35321b6b0be2dad61249202
jandresleiva <andresleiva.4@gmail.com>
2020-07-01 11:57:36 -0300

    Renames UpdateCUIL method and adds IsolatePhones method

--- Updater.cs
+++ Updater.cs
@@ -1,8 +1,12 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg.OpenPgp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using System.Threading.Tasks.Sources;
 using Update_Clients_CUIL.Data;
 
@@ -17,7 +21,7 @@ namespace Update_Clients_CUIL
         /// <param name="from">An int representing a starting row number.</param>
         /// <param name="qty">An int representing the number of simultaneous rows (usually 1) to read.</param>
         /// <returns></returns>
-        public static async Task DoUpdateAsync(int from, int qty)
+        public static async Task UpdateCUILAsync(int from, int qty)
         {
 
             var dbCon = new DBConnection();
@@ -62,6 +66,88 @@ namespace Update_Clients_CUIL
                     if (flagDone == 1)
                         Console.WriteLine(cuil);
                         Console.WriteLine("---- DONE ----");
+                }
+                dbCon.Close();
+            }
+            else
+            {
+                throw new Exception("Couldn't connect to database");
+            }
+        }
+
+
+        public static async Task IsolatePhonesAsync(int from, int qty)
+        {
+
+            var dbCon = new DBConnection();
+            if (dbCon.IsConnect())
+            {
+                var query = "SELECT * FROM digi_new.clientes LIMIT " + from + ", " + qty;
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+                var resultsReader = await cmd.ExecuteReaderAsync();
+                while (await resultsReader.ReadAsync())
+                {
+                    var cliID = resultsReader.GetString(0);
+
+                    var telefonos = new List<string>();
+
+                    if (!resultsReader.IsDBNull(9) && !resultsReader.IsDBNull(10))
+                    {
+                        var ca_telefono = new string(resultsReader.GetString(9).Where(c => char.IsDigit(c)).ToArray());
+                        var telefono = new string(resultsReader.GetString(10).Where(c => char.IsDigit(c)).ToArray());
+                        var tel_description = "telefono";
+                        if (ca_telefono != "" && telefono != "")
+                            telefonos.Add("(" + cliID + ", " + ca_telefono + ", " + telefono + ", '" + tel_description + "')");
+                    }
+                    /*
+                    for (int i = 1; i <= 6; i=i+2)
+                    {
+                        if (!resultsReader.IsDBNull(9 + i) && !resultsReader.IsDBNull(10 + i))
+                        {
+                            var ca_telefono = new string(resultsReader.GetString(9+i).Where(c => char.IsDigit(c)).ToArray());
+                            var telefono = new string(resultsReader.GetString(10+i).Where(c => char.IsDigit(c)).ToArray());
+                            var tel_description = "telefono"+(i-1);
+                            if (telefono != "")
+                                telefonos.Add("(" + cliID + ", " + ca_telefono + ", " + telefono + ", '" + tel_description + "')");
+                        }
+                    }
+                    */
+                    if (!resultsReader.IsDBNull(33) && !resultsReader.IsDBNull(34))
+                    {
+                        var ca_celular = new string(resultsReader.GetString(33).Where(c => char.IsDigit(c)).ToArray());
+                        var celular = new string(resultsReader.GetString(34).Where(c => char.IsDigit(c)).ToArray());
+                        var cel_description = "celular";
+                        if(ca_celular != "" && celular != "")
+                            telefonos.Add("(" + cliID + ", " + ca_celular + ", " + celular + ", '" + cel_description + "')");
+                    }
+
+                    // This var holds a flag to show if the row was indeed modified.
+                    var flagDone = 0;
+
+                    if (telefonos.Count > 0) {
+                        var dbCon2 = new DBConnection();
+                        if (dbCon2.IsConnect())
+                        {
+                            // Runs the update for the given entry and then rise the flag for logging.
+                            var valuesToInsert = string.Join(", ", telefonos);
+                            var query2 = "INSERT digi_new.phones (client, area_code, number, description) VALUES " + valuesToInsert;
+                            var cmd2 = new MySqlCommand(query2, dbCon2.Connection);
+                            await cmd2.ExecuteReaderAsync();
+                            flagDone = 1;
+                            dbCon2.Close();
+                        }
+                        else
+                        {
+                            throw new Exception("Couldn't connect to database");
+                        }
+                    }
+
+                    // The logging is now being made to the console, but could be a text file.
+                    Console.WriteLine(cliID);
+                    if (flagDone == 1)
+                        Console.WriteLine("---- DONE ----");
+                    else
+                        Console.WriteLine("---- FAIL ----");
                 }
                 dbCon.Close();
             }
